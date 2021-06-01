b225ef7947123803c5ae70618b92624f53c53470
Matthew Jury <matthewjury2@gmail.com>
2021-06-01 10:47:00 -0400

    fix __str__ of fight and post models

--- backend/socialmediasite/models.py
+++ backend/socialmediasite/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
-
 class PostComment(models.Model):
     post = models.ForeignKey(
         'Post', on_delete=models.CASCADE, related_name='comments')
@@ -21,9 +18,8 @@ class Fight(models.Model):
     description = models.TextField()
     image_URL = models.TextField()
     date = models.TextField()
-    #posts = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts', blank=True, null=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.title
 
 class Post(models.Model):
@@ -33,5 +29,5 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts', null=True)
     username = models.TextField()
 
-    def _str_(self):
+    def __str__(self):
         return self.fight
