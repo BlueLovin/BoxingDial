73a62fb84b4685335c6e4242393cc74f9c7f79c1
Bluelovin <matthewjury2@gmail.com>
2021-06-05 12:39:40 -0400

    fix post component

--- frontend/src/components/posts/Post.js
+++ frontend/src/components/posts/Post.js
@@ -12,7 +12,7 @@ const Post = props => {
                 <p className="font-weight-light list-group-item bg-light">{post.content}</p>
                 <div className="d-flex text-center justify-content-between text-muted">
                     <p> on: <Link to={`/fight/${post.fight.id}`}>{post.fight.title}</Link></p>
-                    <p><Link to={`/user/${post.owner}`}>view comments</Link></p>
+                    <p><Link to={`/post/${post.id}`}>view comments</Link></p>
                 </div>
             </div>
         </Container>
