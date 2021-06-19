364acf5ee89e12c82e079317d082a9bf5a3c092d
Matthew Jury <matthewjury2@gmail.com>
2021-06-19 10:00:12 -0400

    modify smallfight module

--- frontend/src/components/fights/SmallFight.js
+++ frontend/src/components/fights/SmallFight.js
@@ -1,29 +1,36 @@
-import { Row, Col } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import ShowMoreText from 'react-show-more-text';
+
 const SmallFight = (props) => {
-  let fightData = props.fightData;
-  return (
-    <>
-    <Link to={`/fight/${fightData.id}`}>
-      <h4 className="text-center">{fightData.title}</h4>
-    </Link>
-      <hr />
-      <Row>
-        <Col className="text-center">
-          <img alt="fight" src={fightData.image_URL} className="small-fight-img " />
-        </Col>
-        <hr />
-        <Col>
-          <p>{fightData.description}</p>
-          <p>
-            <b>Date:</b> {fightData.date}
-            <br />
-            <b>Winner:</b> {fightData.result}
-          </p>
-        </Col>
-      </Row>
-    </>
-  );
+	let fightData = props.fightData;
+	return (
+		<>
+			<Link to={`/fight/${fightData.id}`}>
+				<h4 className="text-center">{fightData.title}</h4>
+			</Link>
+			<hr />
+			<Row>
+				<Col className="text-center">
+					<img alt="fight" src={fightData.image_URL} className="small-fight-img " />
+				</Col>
+				<hr />
+				<Col>
+					<p>
+						<b>Date:</b> {fightData.date}
+						<br />
+						<b>Winner:</b> {fightData.result}
+					</p>
+				</Col>
+			</Row>
+			<Row className="p-3 preserve-line-breaks">
+				<ShowMoreText>
+					<p>{fightData.description}</p>
+				</ShowMoreText>
+				<br />
+			</Row>
+		</>
+	);
 };
 
 export default SmallFight;
