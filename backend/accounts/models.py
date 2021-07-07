d7937a6dd1da72bdbcf1c8aab1b9220b076ecfdb
Matthew Jury <matthewjury2@gmail.com>
2021-07-07 15:49:01 -0400

    remove unique=True from UserFollowing model and migrate

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class UserFollowing(models.Model):
-    user_id = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE, unique=True)
+    user_id = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
 
     following_user_id = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
 
